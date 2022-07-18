64c0aedb2ba815fecfaff0faf89aaed670a01245
solnalarmik <solnalarm@ukr.net>
2022-07-18 11:58:45 +0100

    added inject data

--- src/main/java/com/example/customerserviceapp/controller/CustomerController.java
+++ src/main/java/com/example/customerserviceapp/controller/CustomerController.java
@@ -33,6 +33,61 @@ public class CustomerController {
         this.customerMapper = customerMapper;
     }
 
+    @GetMapping("/inject")
+    public void inject() {
+        Customer customer = new Customer();
+        String firstName = "Yuliia";
+        String lastName = "Shevchenko";
+        String address = "London";
+        String postCode = "SE48XA";
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setPostCode(postCode);
+        customer.setAddress(address);
+        customerService.save(customer);
+        Customer customer1 = new Customer();
+        String firstName1 = "Olya";
+        String lastName1 = "Petrova";
+        String postCode1 = "03057";
+        String address1 = "Ukraine, Kyiv";
+        customer1.setFirstName(firstName1);
+        customer1.setLastName(lastName1);
+        customer1.setPostCode(postCode1);
+        customer1.setAddress(address1);
+        Customer customer2 = new Customer();
+        String firstName2 = "Masha";
+        String lastName2 = "Shevchenko";
+        String address2 = "Ukraine, Kyiv";
+        String postCode2 = "03057";
+        customer2.setFirstName(firstName2);
+        customer2.setLastName(lastName2);
+        customer2.setAddress(address2);
+        customer2.setPostCode(postCode2);
+        Customer customer3 = new Customer();
+        String firstName3 = "Elena";
+        String lastName3 = "Petrova";
+        String address3 = "London";
+        String postCode3 = "SE48XA";
+        customer3.setFirstName(firstName3);
+        customer3.setLastName(lastName3);
+        customer3.setPostCode(postCode3);
+        customer3.setAddress(address3);
+        Customer customer4 = new Customer();
+        String firstName4 = "Ivan";
+        String address4 = "London";
+        String postCode4 = "SE48XA";
+        String lastName4 = lastName2;
+        customer4.setFirstName(firstName4);
+        customer4.setLastName(lastName4);
+        customer4.setPostCode(postCode4);
+        customer4.setAddress(address4);
+        customerService.save(customer);
+        customerService.save(customer1);
+        customerService.save(customer2);
+        customerService.save(customer3);
+        customerService.save(customer4);
+    }
+
     @PostMapping("/register")
     public CustomerResponseDto register(@RequestBody @Valid CustomerRequestDto requestDto) {
         ValidatorUtils.checkRequestDto(requestDto);
